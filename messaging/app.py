c93f8e20bd97fada8da7a0d6fe0b622a9593750a
AdamOlevall <adamolevall@gmail.com>
2016-10-11 14:01:20 +0200

    Update app.py

--- messaging/app.py
+++ messaging/app.py
@@ -1,48 +1,63 @@
-#!flask/bin/python
-
-from celery import Celery
-from celery import group
-from tasks import parseTweets
-from flask import Flask, jsonify
-import subprocess
-import sys
 import os
-import swiftclient.client
-import json
 import time
-import urllib2
-from collections import Counter
+import json
+import subprocess
+import swiftclient.client
+from flask import Flask, jsonify
+from tasks import calcPro
+from subprocess import call
+from celery import Celery, subtask, group
 
 app = Flask(__name__)
 
-@app.route('/test', methods=['GET'])
-def print_hello():
-    return 'Tjo Valle! Allt bra? :)', 200
+def summering():
+	startTime = time.time()
+	pronouns = ['han', 'hon', 'den', 'det', 'denna', 'denne', 'hen', 'time']
+	result = [0,0,0,0,0,0,0]
 
-@app.route('/Labb3/messaging', methods=['GET'])
-def cow_say():
-    req = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/tweets")
-    response = urllib2.urlopen(req)
-    tweetsObject = response.read().split()
-    job = group(parseTweets.s([i]) for i in tweetsObject)
-    print tweetsObject
-    tweetTask = job.apply_async()
-    print "Celery is working..."
-    counter = 0
-    while (tweetTask.ready() == False):
-        #print "... %i s" %(counter)
-        counter += 5
-        time.sleep(5)
-    print "The task is done!"
+	parse_list = [calcPro.s('tweets_{}.txt'.format(x)) for x in xrange(0,20)]
+	job = group(parse_list)
 
-    toReturn = tweetTask.get()
+	calc = job.apply_async()
 
-    c = Counter()
-    for d in toReturn:
-        c.update(d)
-    
-    display = dict(c)
-    return jsonify(display), 200
+	while calc.ready() == False:
+		print "sleeping.."
+		time.sleep(2)
+
+	antal = calc.get()
+
+	endTime = time.time()
+	totTime = endTime - startTime
+
+	for i in range(len(result)):
+		for x in range(len(antal)):
+			result[i] = result[i] + antal[x][i]
+	result.append(totTime)
+
+	data = {}
+	for x in range(len(result)):
+		#print pronouns[x] + ": " + str(result[x])
+		data[pronouns[x]] = result[x]
+	json_data = json.dumps(data)
+	return json_data
+
+@app.route('/get', methods=['GET'])
+def downloadTweets():
+	for i in range(0,20):
+		objName = 'tweets_' + str(i) + '.txt'
+		objPath = 'curl -O http://smog.uppmax.uu.se:8080/swift/v1/tweets/'
+		objNamePath = objPath + objName
+		print "getting object " + objName + "..."
+		subprocess.call(objNamePath, shell=True)
+		print "object downloaded"
+
+	json_data = summering()
+	return json_data, 200
+
+@app.route('/run', methods=['GET'])
+def runSummering():
+	return summering(), 200
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+	app.run(host='0.0.0.0',debug= True)
