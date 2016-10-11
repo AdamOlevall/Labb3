14fdc699cddda6439169f41a65acb03e4328d74c
AdamOlevall <adamolevall@gmail.com>
2016-10-11 14:55:48 +0200

    Update tasks.py

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -1,29 +1,38 @@
-import json
-import ujson
-import subprocess
 from celery import Celery
+import os
+import swiftclient.client
+import json
+import time
+import urllib2
+app = Celery('tasks', backend='amqp', broker='amqp://ma:fu@130.238.29.67:22/mafu')
 
-app = Celery('tweet', backend='amqp', broker='amqp://ad:ol@130.238.29.10:5672/adol')
+@app.task()
+def parseTweets(t):
 
-@app.task
-def calcPro(objName):
-	pronouns = ['han', 'hon', 'den', 'det', 'denna', 'denne', 'hen']
-	antal = [0,0,0,0,0,0,0]
-
-	tweetObj = 'curl -O http://smog.uppmax.uu.se:8080/swift/v1/tweets/' + objName
-	print "getting object " + tweetObj + "..."
-	subprocess.call(tweetObj, shell=True)
-
-	print "parsing file: " + objName
-	textJSON = open(objName, 'r')
-
-	for t in textJSON:
+	pronoms={"han": 0, "hon": 0, "den": 0, "det": 0, "denna": 0, "denne": 0, "hen": 0}
+	n = 0 
+	startTime = time.time()
+	req = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/tweets/" + t)
+	response = urllib2.urlopen(req)
+	obj = response.read()
+	objects = open(t, 'w')
+	objects.write(obj)
+	objects.close()
+	objects = open(t, 'r')
+	for line in objects:
 		try:
-			tweet = ujson.loads(t)
-			if 'retweeted_status' not in tweet:
-				for i in range(len(pronouns)):
-					if pronouns[i] in tweet['text'] and not pronouns[i] + 'n' in tweet['text']:
-						antal[i] += 1
+			jL = json.loads(line)
+			tmp1 = jL["text"].lower()
+			tmp = tmp1.split()
+			retweet = jL["retweet_count"]
+			if "retweeted_status" not in jL:
+				n += 1
+				for i in pronoms.keys():
+					if(i in tmp):
+						pronoms[i] += 1
 		except:
 			pass
-	return antal
+	objects.close()
+	time_elapsed = (time.time() - startTime)
+	pronoms.update({"num_of_tweets": n, "time_elapsed":time_elapsed})
+	return pronoms
