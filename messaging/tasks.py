24944360ee8e92245ec9365c564c10909255af0e
AdamOlevall <adamolevall@gmail.com>
2016-10-12 15:31:19 +0200

    Update tasks.py

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -11,7 +11,7 @@ def parseTweets(t):
 
 	pronoms={"han": 0, "hon": 0, "den": 0, "det": 0, "denna": 0, "denne": 0, "hen": 0}
 	n = 0 
-	startTime = time.time()
+	#startTime = time.time()
 	req = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/tweets/" + t)
 	response = urllib2.urlopen(req)
 	obj = response.read()
@@ -33,6 +33,6 @@ def parseTweets(t):
 		except:
 			pass
 	objects.close()
-	time_elapsed = (time.time() - startTime)
-	pronoms.update({"num_of_tweets": n, "time_elapsed":time_elapsed})
+	#time_elapsed = (time.time() - startTime)
+	pronoms.update({"num_of_tweets": n})
 	return pronoms
