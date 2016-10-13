d5b4ed4a30b9dd41592bd092d4da944b5bcc9929
AdamOlevall <adamolevall@gmail.com>
2016-10-13 10:37:04 +0200

    Update tasks.py

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -4,7 +4,8 @@ import swiftclient.client
 import json
 import time
 import urllib2
-app = Celery('tasks', backend='amqp', broker='amqp://ad:ol@130.238.29.13:5672/adol')
+#app = Celery('tasks', backend='amqp', broker='amqp://ad:ol@130.238.29.13:5672/adol')
+app = Celery('tasks', backend='amqp', broker='amqp://guest:guest@localhost:5672//')
 
 @app.task()
 def parseTweets(t):
